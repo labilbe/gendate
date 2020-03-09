4d91f542ee469cd23bd18674c5ba5034f1123f3f
Franck Quintana <franck.quintana@gmail.com>
2020-03-09 11:51:10 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace gendate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	var firstDate = new DateTime(1900, 1, 1);
+        	var lastDate = new DateTime(2020, 03, 09);
+
+    		var formats = new[] 
+    		{ 
+    			"ddMMyyyy",
+    			"ddMMyy",    			
+    			"dd/MM/yyyy",
+    			"dd/MM/yy",
+    			"dd-MM-yyyy",
+    			"dd-MM-yy",    			
+    		};
+
+			foreach (var format in formats)
+        		for (var date = firstDate; date <= lastDate; date = date.AddDays(1))        		
+            		Console.WriteLine(date.ToString(format));
+        }
+    }
+}
